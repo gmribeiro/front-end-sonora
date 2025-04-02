05b572e59f7f113266314161eb1fab98050a7e2b
Gabriel Ribeiro <146293875+gmribeiro@users.noreply.github.com>
2025-04-02 20:38:22 -0300

    header maior

--- frontsonora/src/components/header/header.jsx
+++ frontsonora/src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import './header.css';
 import { Link } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
@@ -13,14 +13,22 @@ function Header() {
           <input type="search" name="search" className='search' placeholder='Encontre seu estilo favorito'/>
 
           <nav className='links'>
-            <Link className='link' to="/">HOME</Link>
-            <Link className='link' to="/MeusConvites">MEUS CONVITES</Link>
-            <Link className='link' id='btn-entrar' to="/Acesso">ENTRAR</Link>
+            <Link className='link' to="/">
+              <span>HOME</span>
+              <div className='underline'></div>
+            </Link>
+            <Link className='link' to="/MeusConvites">
+              <span>MEUS CONVITES</span>
+              <div className='underline'></div>
+            </Link>
+            <Link id='btn-entrar' to="/Acesso">
+              <span>ENTRAR</span>
+              <div className='underline'></div>
+            </Link>
           </nav>
-
         </div>
       </header>
-  )
+  );
 }
 
 export default Header;
