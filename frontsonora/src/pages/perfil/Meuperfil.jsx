df6ff8053bc4900a1b7987f1aa1f7d6a9dcff68a
Gabriel Ribeiro <gabrielmr.profissional@gmail.com>
2025-05-07 20:37:14 -0300

    arrumando alguns bugs

--- frontsonora/src/pages/perfil/Meuperfil.jsx
+++ frontsonora/src/pages/perfil/Meuperfil.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import './meuperfil.css';
+import '../css/meuperfil.css';
 
 function MeuPerfil() {
   const [nomeUsuario, setNomeUsuario] = useState('');
