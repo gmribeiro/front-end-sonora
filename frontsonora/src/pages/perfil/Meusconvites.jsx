7cb462fea8688364d77ab14681ecf9a791be28f5
gpego1 <gpego786@gmail.com>
2025-05-20 21:47:18 -0300

    GET de ávaliacoes.jsx' mudado

--- frontsonora/src/pages/perfil/Meusconvites.jsx
+++ frontsonora/src/pages/perfil/Meusconvites.jsx
@@ -3,7 +3,6 @@ import Footer from '../../components/Footer/footer';
 import '../css/global.css';
 import React from 'react';
 import useTitle from '../../hooks/useTitle.js';
-// import Eventos from '../../components/Eventos/eventos.jsx'; // You might not need this here
 import Dashboard from '../../components/MeusEventos/Dashboard.jsx';
 
 function Meusconvites() {
