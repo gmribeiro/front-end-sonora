c7b8fe5e21210a152ea014a6cbccc1b0b671bc21
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2025-06-10 19:50:38 -0300

    Create App.jsx

---
+++ frontsonora/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({title: '', body: ''});
+  const [isTokenFound, setTokenFound] = useState(false);
+  myGetToken(setTokenFound);
+
+
+}
