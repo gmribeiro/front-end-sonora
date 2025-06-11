fb3b9a9934127736fe91bd55624d611bb57266e3
fssilva <fillipe.silva@fiec.edu.br>
2025-06-10 22:58:16 -0300

    Fixing

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
 
  // Load app-level env vars to node-level env vars.
  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+ const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
