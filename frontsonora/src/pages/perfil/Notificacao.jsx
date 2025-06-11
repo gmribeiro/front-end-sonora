8918628e77f517e75a9281ae6da38b25abc9dbf7
fssilva <fillipe.silva@fiec.edu.br>
2025-06-10 21:33:58 -0300

    Fix import 2

--- frontsonora/src/pages/perfil/Notificacao.jsx
+++ frontsonora/src/pages/perfil/Notificacao.jsx
@@ -1,15 +1,15 @@
-import Header from '../../components/Header/header.jsx';
-import Footer from '../../components/Footer/footer.jsx';
+
 import '../css/global.css';
 import React from 'react';
 import Notificacao from '../../components/Notificacao/notificacao.jsx';
 import useTitle from '../../hooks/useTitle.js';
+import Header from '../../components/header/header.jsx';
 
 function Meusconvites() {
   useTitle('Notificações - Sonora');
   return (
     <>
-        <Header/>
+        <Header />
         <Notificacao/>
     </>
   )
