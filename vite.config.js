8448c037aa4b82d8b3a7ddd6a1b721a16585c1a5
fssilva <fillipe.silva@fiec.edu.br>
2025-06-10 22:43:48 -0300

    Rolling back

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL;
+const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
  // Load app-level env vars to node-level env vars.
  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
