a31458023818d24226e9b567f080a2a3c0ebaef1
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2025-06-12 09:24:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,63 +10,64 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
+  const backendUrl = import.meta.env.VITE_APP_BACKEND_URL;
 return {
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/auth': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/genres': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/places': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/eventos': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/reservas': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/avaliacoes': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/musicos': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/notifications': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/contratos': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/users': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       },
       '/escalas': {
-        target: process.env.VITE_APP_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false
       }
